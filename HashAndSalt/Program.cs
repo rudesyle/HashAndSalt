6da9af59409119ecfa9b196a1c0f4b9f2c90405e
Rudesyle <rudesyle@gmail.com>
2014-11-21 12:41:32 -0500

    More committing

--- HashAndSalt/Program.cs
+++ HashAndSalt/Program.cs
@@ -10,6 +10,15 @@ namespace HashAndSalt
     {
         static void Main(string[] args)
         {
+            string hash, salt;
+            const string password = "!~)(InYourThighs712";
+
+            PasswordHash.CreateHash(password, out hash, out salt);
+
+            Console.WriteLine(hash);
+            Console.WriteLine(salt);
+            Console.WriteLine(PasswordHash.ValidatePassword(password,hash));
+            Console.ReadLine();
         }
     }
 }
