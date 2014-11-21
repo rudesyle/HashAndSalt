2eb05a1253151599fe9c77137a874581e9b665a5
Rudesyle <rudesyle@gmail.com>
2014-11-21 12:27:16 -0500

    Commiment

---
+++ HashAndSalt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashAndSalt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
